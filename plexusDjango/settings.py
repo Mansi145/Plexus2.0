5ad7f720cf61ce079be88573b8cdded99ecd4575
Mansi145 <guptamansi9350@gmail.com>
2020-05-18 08:28:55 +0530

    cors headers implemented

--- plexusDjango/settings.py
+++ plexusDjango/settings.py
@@ -33,18 +33,25 @@ INSTALLED_APPS = [
 
     # Rest Framework
     'rest_framework',
+
+    # CORS
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    # CORS Middleware
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'plexusDjango.urls'
 
@@ -156,4 +163,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 django_heroku.settings(locals())
-
